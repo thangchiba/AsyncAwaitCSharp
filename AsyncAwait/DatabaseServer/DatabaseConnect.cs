c049548467bd6025aaf6ff86bf3e7a02b3dfe0f5
ThangChiba <thangchiba@gmail.com>
2022-10-18 21:24:18 +0900

    Connect To DB

---
+++ AsyncAwait/DatabaseServer/DatabaseConnect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Npgsql;
+
+namespace DatabaseServer
+{
+    public class DatabaseConnect
+    {
+        // Obtain connection string information from the portal
+        private static string Host = "35.243.99.138";
+        private static string User = "thangchiba";
+        private static string DBname = "MMORPG";
+        private static string Password = "Hoangthang123$";
+        private static string Port = "5432";
+
+        public void Connect()
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+
+            using (var conn = new NpgsqlConnection(connString))
+
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS inventory", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished dropping table (if existed)");
+
+                }
+
+                using (var command = new NpgsqlCommand("CREATE TABLE inventory(id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER)", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished creating table");
+                }
+
+                using (var command = new NpgsqlCommand("INSERT INTO inventory (name, quantity) VALUES (@n1, @q1), (@n2, @q2), (@n3, @q3)", conn))
+                {
+                    command.Parameters.AddWithValue("n1", "bananaaaa");
+                    command.Parameters.AddWithValue("q1", 150);
+                    command.Parameters.AddWithValue("n2", "orange");
+                    command.Parameters.AddWithValue("q2", 154);
+                    command.Parameters.AddWithValue("n3", "apple");
+                    command.Parameters.AddWithValue("q3", 100);
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+                }
+            }
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AsyncAwait/DatabaseServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseServer;
+
+namespace DatabaseServer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DatabaseConnect db = new DatabaseConnect();
+            db.Connect();
+        }
+    }
+}
